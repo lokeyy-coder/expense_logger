7cf9ebe9452229500c31f6981b50e2463d7440bc
lokeyy-coder <andrew.loke1996@gmail.com>
2025-12-30 17:40:07 +1100

    Transaction table debug 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ExpenseLogger from './ExpenseLogger'; // Adjust the path if ExpenseLogger.js is in a different folder
+import './App.css';
+import ExpenseLogger from './ExpenseLogger';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="bg-blue-500 text-white py-4 text-center">
-        <h1 className="text-2xl font-bold">Expense Tracker</h1>
-      </header>
-      <main className="p-4">
-        <ExpenseLogger />
-      </main>
+      <ExpenseLogger />
     </div>
   );
 };
 
 export default App;
+
